18ae505d2b07798b33dd4c018db54ab846fe1bb1
Krisztián Korpa <krisztian@datatovalue.nl>
2023-06-01 12:08:49 +0000

    stg_session_acquisiton table created

---
+++ includes/constants.js
@@ -0,0 +1,6 @@
+const GA4_DATABASE = 'datatovaluenl-ga4-bigquery';
+const GA4_DATASET = 'analytics_206569036';
+const GA4_TABLE = 'events_*';
+const START_DATE = "'2022-03-31'";
+
+module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, START_DATE}
